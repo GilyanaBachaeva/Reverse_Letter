c76a9e26a7730df408f09148650561d4290de089
Gilyana Bachaeva <gelchik29@icloud.com>
2025-05-13 20:29:48 +0300

    Add spacing between @Test annotations and methods, remove unnecessary throws Exception declarations

--- untitled/src/test/java/com/example/AppTest.java
+++ untitled/src/test/java/com/example/AppTest.java
@@ -6,78 +6,89 @@ import static com.example.StringUtil.reverseLetters;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
+
     @Test
-    void reverseLetters_shouldReverseLetters_keepingSymbolsInPosition() throws Exception {
+    void reverseLetters_shouldReverseLetters_keepingSymbolsInPosition() {
         String result = reverseLetters("J@va the be$t!123");
         String expectedReversedString = "t@eb eht av$J!123";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-     void reverseLetters_shouldReturnEmptyString_ifContainsEmptyString() throws Exception {
+    void reverseLetters_shouldReturnEmptyString_ifContainsEmptyString() {
         String result = reverseLetters("");
         String expectedReversedString = "";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReturnNull_ifContainsNull() throws Exception {
+    void reverseLetters_shouldReturnNull_ifContainsNull() {
         String result = reverseLetters(null);
         String expectedReversedString = null;
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReturnSameLetter_ifContainsSingleLetter() throws Exception {
+    void reverseLetters_shouldReturnSameLetter_ifContainsSingleLetter() {
         String result = reverseLetters("Q");
         String expectedReversedString = "Q";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReturnSameCharacter_ifContainsSingleCharacter() throws Exception {
+    void reverseLetters_shouldReturnSameCharacter_ifContainsSingleCharacter() {
         String result = reverseLetters("*");
         String expectedReversedString = "*";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReturnSameString_ifContainsEvenNumberOfCharacters() throws Exception {
+    void reverseLetters_shouldReturnSameString_ifContainsEvenNumberOfCharacters() {
         String result = reverseLetters("@!123#)^");
         String expectedReversedString = "@!123#)^";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReturnSameString_ifContainsOddNumberOfCharacters() throws Exception {
+    void reverseLetters_shouldReturnSameString_ifContainsOddNumberOfCharacters() {
         String result = reverseLetters("}?<&#");
         String expectedReversedString = "}?<&#";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReverseLetters_ifContainsEvenNumberOfLetters() throws Exception {
+    void reverseLetters_shouldReverseLetters_ifContainsEvenNumberOfLetters() {
         String result = reverseLetters("Hello world");
         String expectedReversedString = "dlrow olleH";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReverseLetters_ifContainsOddNumberOfLetters() throws Exception {
+    void reverseLetters_shouldReverseLetters_ifContainsOddNumberOfLetters() {
         String result = reverseLetters("Java is the best");
         String expectedReversedString = "tseb eh tsi avaJ";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReturnSameString_ifContainsOnlyCharacters() throws Exception {
+    void reverseLetters_shouldReturnSameString_ifContainsOnlyCharacters() {
         String result = reverseLetters("=568£&$");
         String expectedReversedString = "=568£&$";
 
         assertEquals(expectedReversedString, result);
     }
+
     @Test
-    void reverseLetters_shouldReverseLetters_ifContainsOnlyLetters() throws Exception {
+    void reverseLetters_shouldReverseLetters_ifContainsOnlyLetters() {
         String result = reverseLetters("Valhalla");
         String expectedReversedString = "allahlaV";
 
